e641741a1455d253f4acc97b9bdaa7c425a6c9a0
Steven Hopkins <shopkins@shutterfly.com>
2016-01-06 10:24:42 -0700

    Added prefix flag and walking json

--- docker2graphite.go
+++ docker2graphite.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
+	"log"
 	"net"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
 func perror(err error) {
@@ -25,14 +29,37 @@ func getJSON(c http.Client, url string, target *interface{}) error {
 	return json.NewDecoder(r.Body).Decode(&target)
 }
 
+func safeString(s string) string {
+	reg, err := regexp.Compile("[^A-Za-z0-9]+")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	safe := reg.ReplaceAllString(s, "_")
+	safe = strings.ToLower(strings.Trim(safe, "_"))
+	return safe
+}
+
 func main() {
+	var prefix = flag.String("prefix", "docker2graphite", "Graphite prefix for metric names e.g. docker2graphite.prod")
+	flag.Parse()
+
 	tr := &http.Transport{
 		Dial: fakeDial,
 	}
 	client := &http.Client{Transport: tr}
 
 	var data interface{}
-	err := getJSON(*client, "http://unix.sock/version", &data) //containers/json
+	err := getJSON(*client, "http://unix.sock/containers/json", &data)
 	perror(err)
-	fmt.Println(data)
+
+	m := data.([]interface{})
+	for _, i := range m {
+		d := i.(map[string]interface{})
+		for k, v := range d {
+			if k == "Names" {
+				z := v.([]interface{})
+				fmt.Printf("%v.%v.running 1\n", *prefix, safeString(z[0].(string)))
+			}
+		}
+	}
 }
